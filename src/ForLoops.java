c0669205f0d294eea53528b5321b3e7d855c6199
emerzon <emerzondx@gmail.com>
2017-10-30 20:42:02 -0500

    Initial commit

---
+++ src/ForLoops.java
@@ -0,0 +1,11 @@
+// Print 1 to 10 with the word Number in a for loop 
+
+public class ForLoops {
+    public static void main(String [] args){
+
+        for (int i = 1 ; i < 11; i++ ) {
+            System.out.println("Number " + i);
+        }
+
+    }
+}
